693507f70c423c5562b6d5b9a52e6717d0ba181b
shubhamcse59 <shubham.mayur@gmail.com>
2022-07-08 00:43:48 +0530

    initial myapp commit

---
+++ my-app/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+
+
+const NavBar= ({totalCounters}) =>{
+    return (<nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="/">
+    NavBar{" "}
+    <span className="badge badge-pill badge-secondary"> 
+        {totalCounters}
+    </span>
+    </a>
+</nav>);
+};
+
+
+export default NavBar;
